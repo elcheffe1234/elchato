1ebc2febee645b12c416c7f1201cee92437101ff
elcheffe1234 <asdf@asdf.ch>
2016-06-21 14:10:49 +0200

    filewrite added

--- app/src/main/java/elchatocompany/elchato/AddActivity.java
+++ app/src/main/java/elchatocompany/elchato/AddActivity.java
@@ -1,5 +1,6 @@
 package elchatocompany.elchato;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -25,6 +30,9 @@ public class AddActivity extends AppCompatActivity {
         //if user exists
         if(!sp.getString(user, "").isEmpty()){
            //TODO: add user to listitem in chatview
+
+            savePersonInChatsFile(user, "actual_user_chat");
+
             Toast.makeText(this, "Person added", Toast.LENGTH_SHORT).show();
             finish();
         }
@@ -36,5 +44,16 @@ public class AddActivity extends AppCompatActivity {
     public void cancel(View v){
         Toast.makeText(this, "Adding person canceled", Toast.LENGTH_SHORT).show();
         finish();
+    }
+
+    public void savePersonInChatsFile(String addedPerson, String filename){
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(filename, this.MODE_PRIVATE);
+            fos.write(addedPerson.getBytes());
+            fos.close();
+        } catch (IOException e){
+            Toast.makeText(this, "Unable to add new chat!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
