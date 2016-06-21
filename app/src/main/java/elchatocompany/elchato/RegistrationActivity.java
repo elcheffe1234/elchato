57e736a90523542caaf72aa0057c57698b05fd13
elcheffe1234 <asdf@asdf.ch>
2016-06-21 16:06:09 +0200

    added username check

--- app/src/main/java/elchatocompany/elchato/RegistrationActivity.java
+++ app/src/main/java/elchatocompany/elchato/RegistrationActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        sp = PreferenceManager
+                .getDefaultSharedPreferences(this);
     }
 
     public void create(View v){
@@ -21,18 +25,18 @@ public class RegistrationActivity extends AppCompatActivity {
         EditText user = (EditText) findViewById(R.id.user);
         EditText password = (EditText) findViewById(R.id.password);
 
-
-
-
-        save(user.getText().toString(), password.getText().toString());
-        save(user.getText().toString() + "_email", email.getText().toString());
-
-        finish();
+        if(sp.getString(user.getText().toString(), "").isEmpty()) {
+            save(user.getText().toString(), password.getText().toString());
+            save(user.getText().toString() + "_email", email.getText().toString());
+            finish();
+        }
+        else{
+            Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void save(String key, String value) {
-        SharedPreferences sp = PreferenceManager
-                .getDefaultSharedPreferences(this);
+
 
         SharedPreferences.Editor edit = sp.edit();
         edit.putString(key, value);
