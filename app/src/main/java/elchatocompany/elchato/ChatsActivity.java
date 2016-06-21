35e5b28a6e12fd75cfde02fa623d0b2de2f62169
elcheffe1234 <asdf@asdf.ch>
2016-06-21 11:41:01 +0200

    added addActivity structure

--- app/src/main/java/elchatocompany/elchato/ChatsActivity.java
+++ app/src/main/java/elchatocompany/elchato/ChatsActivity.java
@@ -1,7 +1,9 @@
 package elchatocompany.elchato;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChatsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ChatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
+    }
+
+    public void addChat(View v){
+        Intent i = new Intent(this, AddActivity.class);
+        startActivity(i);
     }
 }
