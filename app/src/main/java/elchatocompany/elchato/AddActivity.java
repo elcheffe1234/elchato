932a00d47beab15cc47b15c33c3c55e3d088c8ce
elcheffe1234 <asdf@asdf.ch>
2016-06-21 15:40:21 +0200

    filewrite added

--- app/src/main/java/elchatocompany/elchato/AddActivity.java
+++ app/src/main/java/elchatocompany/elchato/AddActivity.java
@@ -1,17 +1,23 @@
 package elchatocompany.elchato;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -34,6 +40,8 @@ public class AddActivity extends AppCompatActivity {
             savePersonInChatsFile(user, "actual_user_chat");
 
             Toast.makeText(this, "Person added", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, ChatsActivity.class);
+            startActivity(i);
             finish();
         }
         else {
@@ -43,17 +51,65 @@ public class AddActivity extends AppCompatActivity {
 
     public void cancel(View v){
         Toast.makeText(this, "Adding person canceled", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, ChatsActivity.class);
+        startActivity(i);
         finish();
     }
 
     public void savePersonInChatsFile(String addedPerson, String filename){
         FileOutputStream fos = null;
         try {
-            fos = openFileOutput(filename, this.MODE_PRIVATE);
-            fos.write(addedPerson.getBytes());
+            String writestring = readFromFile();
+            if(!writestring.isEmpty()) {
+                writestring = writestring + "," + addedPerson;
+            }
+            else {
+                writestring = addedPerson;
+            }
+            fos = openFileOutput("yyy.txt", this.MODE_PRIVATE);
+            fos.write(writestring.getBytes());
             fos.close();
         } catch (IOException e){
             Toast.makeText(this, "Unable to add new chat!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private String readFromFile() {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = openFileInput("yyy.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent i = new Intent(this, ChatsActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/elchatocompany/elchato/ChatsActivity.java
+++ app/src/main/java/elchatocompany/elchato/ChatsActivity.java
@@ -2,43 +2,66 @@ package elchatocompany.elchato;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ChatsActivity extends AppCompatActivity {
 
     private ListView listview;
+    String[] values;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
         showList();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void addChat(View v){
+
+
+    public void addChat(View v) {
         Intent i = new Intent(this, AddActivity.class);
         startActivity(i);
+        finish();
     }
 
-    public void openChat(){
+    public void openChat() {
         Intent i = new Intent(this, ChatActivity.class);
         startActivity(i);
     }
 
-    public void showList(){
+    public void showList() {
         listview = (ListView) findViewById(R.id.listView);
 
-        String[] values = new String[] {
-                "Android List View",
-                "Adapter implementation"
-        };
+        values = readFromFile();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, values);
@@ -48,20 +71,83 @@ public class ChatsActivity extends AppCompatActivity {
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // ListView Clicked item index
-                int itemPosition     = position;
-
+                int itemPosition = position;
                 // ListView Clicked item value
-                String  itemValue    = (String) listview.getItemAtPosition(position);
-
+                String itemValue = (String) listview.getItemAtPosition(position);
                 openChat();
-
             }
-
         });
+    }
 
+    public String[] readFromFile() {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = openFileInput("yyy.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+        String[] contacts = ret.split(",");
+        return contacts;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Chats Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://elchatocompany.elchato/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Chats Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://elchatocompany.elchato/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
+
